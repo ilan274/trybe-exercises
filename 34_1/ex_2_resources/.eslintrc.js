d4b38b65f9cd3b0f7ba2d5ca2998e78a29d30e07
Ilan Herbach <ilan.herbach@gmail.com>
2021-01-11 07:10:15 -0300

    Config file eslintrc added

---
+++ 34_1/ex_2_resources/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'always',
+      asyncArrow: 'always',
+    }],
+  },
+};
