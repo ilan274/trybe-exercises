d6ed9199448b61b93d7ac915341f7ea7d2eae7fa
Ilan Herbach <ilan.herbach@gmail.com>
2021-01-14 15:56:38 -0300

    Fix line length error

--- 35_1/training-exercises.py
+++ 35_1/training-exercises.py
@@ -1,4 +1,7 @@
-# Exercício 1: No terminal, inicialize duas variáveis a e b, sendo a = 10 e b = 5. Mostre o resultado das 7 operações básicas (soma, subtração, multiplicação, divisão, divisão inteira, potenciação e módulo) envolvendo essas variáveis.
+# Exercício 1: No terminal, inicialize duas variáveis a e b, sendo
+# a = 10 e b = 5. Mostre o resultado das 7 operações básicas
+# (soma, subtração, multiplicação, divisão, divisão inteira,
+# potenciação e módulo) envolvendo essas variáveis.
 a = 10
 b = 5
 print(a + b, a - b, a * b, a / b, a // b, a ** b, a % b)
