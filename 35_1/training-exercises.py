baacccd3bd3dd4ab5b02d33ca26bc4e6efe49157
Ilan Herbach <ilan.herbach@gmail.com>
2021-01-15 18:13:07 -0300

    Range examples Python

--- 35_1/training-exercises.py
+++ 35_1/training-exercises.py
@@ -103,3 +103,15 @@ print(info)
 # e imprima o objeto no console.
 info.pop("origem")
 print(info)
+
+# Range (range) / range( [start], stop[, step] )
+# definimos somente o valor de parada
+
+list(range(5))  # saída: [0, 1, 2, 3, 4]
+# definimos o valor inicial e o de parada
+list(range(1, 6))  # saída: [1, 2, 3, 4, 5]
+# definimos valor inicial, de parada e modificamos o passo para 2
+list(range(1, 11, 2))  # saída: [1, 3, ,5 ,7 , 9]
+# podemos utilizar valores negativos para as entradas também
+list(range(10, 0, -1))  # saída: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+print(list(range(0, -10, -1)))
