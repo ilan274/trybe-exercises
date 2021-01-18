1751d6df504486f63242214e1144e930e31ee43b
Ilan Herbach <ilan.herbach@gmail.com>
2021-01-18 10:28:52 -0300

    Exercise 02 print asterisks times number

---
+++ 35_1/exercise03.py
@@ -0,0 +1,8 @@
+# Exercício 3: Faça um programa que, dado um valor n qualquer, tal que n > 1,
+# imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
+# Por exemplo:
+
+
+def asterisks(num):
+    for _i in range(num):
+        print("*" * num)
