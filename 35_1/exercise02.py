f77978361d99a7793b54d68ce72b5e830953731c
Ilan Herbach <ilan.herbach@gmail.com>
2021-01-18 09:46:14 -0300

    Calculate the average Python

---
+++ 35_1/exercise02.py
@@ -0,0 +1,8 @@
+# Exercício 2: Calcule a média aritmética dos valores contidos em uma lista.
+
+
+def average(numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total
