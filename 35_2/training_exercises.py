7bb6358e5977d69d3b598aaca249479bf7cb593d
Ilan Herbach <ilan.herbach@gmail.com>
2021-01-20 11:21:44 -0300

    Opening and closing files with 'try except else' exception

--- 35_2/training_exercises.py
+++ 35_2/training_exercises.py
@@ -19,3 +19,16 @@ for num in numbers:
     else:
         total += int(num)
 print(total)
+
+try:
+    arquivo = open("arquivo.txt", "r")
+except OSError:
+    # será executado caso haja uma exceção
+    print("arquivo inexistente")
+else:
+    # será executado se tudo ocorrer bem no try
+    print("arquivo manipulado e fechado com sucesso")
+    arquivo.close()
+finally:
+    # será sempre executado, independentemente de erro
+    print("Tentativa de abrir arquivo")
