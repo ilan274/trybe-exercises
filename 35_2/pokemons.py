6e5eb7b4dd6b4a21418bdddfdb33610a088cdec3
Ilan Herbach <ilan.herbach@gmail.com>
2021-01-20 14:33:33 -0300

    Loading JSON using 'load' instead of 'loads'

--- 35_2/pokemons.py
+++ 35_2/pokemons.py
@@ -8,3 +8,11 @@ with open("pokemons.json") as file:
     # "load" will load a JSON from file while "loads" will from "text"
 
 print(pokemons[0])
+
+# another way of reading JSON
+
+with open("pokemons.json") as file:
+    pokemons = json.load(file)["results"]
+    # here we're loading the file
+    # that's why we're using load instead
+print(pokemons[0])
