7f24400e4a2016b1c44fa1eb8f16199e270e7820
Ilan Herbach <ilan.herbach@gmail.com>
2021-01-28 15:11:36 -0300

    Exemplo programação estruturada em Python

---
+++ 36_1/exemplo_estrutuado.py
@@ -0,0 +1,25 @@
+# Estruturado: É um paradigma composto apenas por estruturas de programação
+# básicas, como variáveis, funções, módulos, condicionais e laços de repetição.
+# Não possuindo formas mais complexas de organização de código.
+# Exemplo
+
+
+def sum(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+result = sum(2, 2)
+
+print("Resultado de 2 + 2:", result)
